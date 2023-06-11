5ea7c4e362dd89fdf1eca915c9a9874c29666aad
Brix John Novesteras <brixjohnovesteras@gmail.com>
2023-06-11 15:58:12 +0800

    setup server and client

---
+++ client/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import  ReactDOM  from 'react-dom'
+
+import App from '/App'
+
+//Basic boilerplate code need for every application
+ReactDOM.render(<App/>, document.querySelector('#root'));
